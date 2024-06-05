a1caf04cd5ad2da4d023941fa25ddfd588e8683e
lucas <developer.lucas@pm.me>
2024-06-04 23:44:26 -0400

    feat: setup file structure, added topnav component

---
+++ src/app/_components/topnav.jsx
@@ -0,0 +1,7 @@
+export function TopNav() {
+  return (
+    <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
+      <div>NextDrive</div>
+    </nav>
+  );
+}
