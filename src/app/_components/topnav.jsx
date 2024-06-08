0734628e514be6880ff8ac39ebb792d9abbe5571
lucas <developer.lucas@pm.me>
2024-06-08 19:20:53 -0400

    feat: added link to root page on logo in top navbar

--- src/app/_components/topnav.jsx
+++ src/app/_components/topnav.jsx
@@ -6,7 +6,9 @@ export function TopNav() {
   return (
     <div className="border-b">
       <nav className="mx-auto flex max-w-screen-2xl items-center justify-between p-2.5 text-xl font-semibold ">
-        <div>Glimpse</div>
+        <div>
+          <Link href="/">Glimpse</Link>
+        </div>
         <section className="flex space-x-4 items-center">
           <Button variant="ghost" size="icon">
             <Link href="https://github.com/ay-Lucas">
