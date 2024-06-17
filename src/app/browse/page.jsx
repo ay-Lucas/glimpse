fc99d379cc616216d2a6d082c4a23f21b9557b8e
lucas <developer.lucas@pm.me>
2024-06-17 16:51:15 -0400

    feat: added descending popularity sort. refactor: deleted poster component, split up functions into /browse/page.jsx

--- src/app/browse/page.jsx
+++ src/app/browse/page.jsx
@@ -1,19 +1,67 @@
 import { ImageCarousel } from "./_components/image-carousel";
-// import { getBackgrounds } from "@/components/backdrops";
-import { getBackgrounds } from "./_components/posters";
+import { options, shuffle, bubbleSort } from "@/lib/utils";
+
+const baseImageUrl = "https://image.tmdb.org/t/p/original";
+const baseUrl = "https://api.themoviedb.org/3";
+const MIN_POPULARITY = 200;
+const MIN_TRENDING_POPULARITY = 100;
+
+// time window: day or week
+async function getTrendingTv(timeWindow) {
+  const res = await fetch(`${baseUrl}/trending/tv/${timeWindow}`, options);
+  return res.json();
+}
+
+async function getTrendingMovies(timeWindow) {
+  const res = await fetch(`${baseUrl}/trending/movie/${timeWindow}`, options);
+  return res.json();
+}
+
+function sortPopular(array, minPopularity = 0) {
+  if (typeof array === undefined || array.length == 0)
+    throw new Error("Could not sort array: array is undefined or empty");
+  bubbleSort(array, array.length);
+  if (minPopularity > 0)
+    array = array.filter((item) => item.popularity >= minPopularity);
+  return array;
+}
 
 export default async function HomePage() {
-  const posters = await getBackgrounds();
-  const both = posters.both;
-  const tv = posters.tv;
-  const movies = posters.movies;
+  const [tvRes, movieRes] = await Promise.all([
+    getTrendingTv("day"),
+    getTrendingMovies("day"),
+  ]);
+  const trendingTv = sortPopular(tvRes.results, MIN_POPULARITY);
+  const trendingMovies = sortPopular(movieRes.results, MIN_POPULARITY);
+
+  const trendingTvAndMovies = tvRes.results.concat(movieRes.results);
+  const trending = sortPopular(trendingTvAndMovies, MIN_TRENDING_POPULARITY);
+  console.log(trendingTv);
+  // const trending = shuffle(trendingTvAndMovies);
+
   return (
     <main className="mt-1">
       <div className="mx-auto space-y-4 overflow-visible">
-        {/* <h2 className="lg:ml-20 ml-0">Trending</h2> */}
-        <ImageCarousel images={both} title="Trending" />
-        <ImageCarousel images={tv} title="TV Shows" />
-        <ImageCarousel images={movies} title="Movies" />
+        <ImageCarousel
+          images={
+            trending.map((item) => `${baseImageUrl}${item.poster_path}`) ?? []
+          }
+          title="Trending"
+        />
+        <ImageCarousel
+          images={
+            trendingTv.map((show) => `${baseImageUrl}${show.poster_path}`) ?? []
+          }
+          title="TV Shows"
+        />
+        <ImageCarousel
+          images={
+            trendingMovies.map(
+              (movie) => `${baseImageUrl}${movie.poster_path}`,
+            ) ?? []
+          }
+          title="Movies"
+        />
       </div>
     </main>
   );
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -22,3 +22,21 @@ export const options = {
   },
   cache: "force-cache",
 };
+
+// sorts trending movies and tv objects
+export function bubbleSort(arr, n) {
+  let i, j, temp;
+  let swapped;
+  for (i = 0; i < n - 1; i++) {
+    swapped = false;
+    for (j = 0; j < n - i - 1; j++) {
+      if (arr[j].popularity < arr[j + 1].popularity) {
+        temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        swapped = true;
+      }
+    }
+    if (swapped == false) break;
+  }
+}
