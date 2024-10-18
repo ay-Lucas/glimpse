7e42723fe549a82791b2e490d742ded6c8942d46
Lucas <developer.lucas@pm.me>
2024-10-18 14:22:36 -0400

    fix: fixed no backdrops showing on login page

--- src/app/login/_components/backdrops.tsx
+++ src/app/login/_components/backdrops.tsx
@@ -3,7 +3,7 @@ import { baseApiUrl, options } from "@/lib/constants";
 
 const trendingTvUrl = `${baseApiUrl}/trending/tv/day?language=en-US`;
 const trendingMoviesUrl = `${baseApiUrl}/trending/movie/day?language=en-US`;
-const MAX_POPULARITY = 250;
+const MIN_POPULARITY = 10;
 
 export async function getBackgrounds() {
   let [res1, res2] = await Promise.all([
@@ -14,8 +14,8 @@ export async function getBackgrounds() {
   const backdropUrls = [];
   for (let i in res1.results) {
     if (
-      res1.results[i].popularity > MAX_POPULARITY &&
-      res2.results[i].popularity > MAX_POPULARITY
+      res1.results[i].popularity > MIN_POPULARITY &&
+      res2.results[i].popularity > MIN_POPULARITY
     )
       backdropUrls.push(
         `https://image.tmdb.org/t/p/original${res1.results[i].backdrop_path}`,
