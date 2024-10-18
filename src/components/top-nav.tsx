e868959b5dfdda6fc34604c33f3a2317b3f106ae
Lucas <developer.lucas@pm.me>
2024-10-18 16:47:59 -0400

    fix: fixed topnav check for if user is signed in

--- src/components/top-nav.tsx
+++ src/components/top-nav.tsx
@@ -2,8 +2,10 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { AvatarDropdown } from "./avatar-dropdown";
 import { Search } from "./search";
+import { auth } from "@/db/auth";
 
-export async function TopNav({ signedIn = false }) {
+export async function TopNav() {
+  const session = await auth();
   return (
     <div className="sticky top-0 left-0 border-b border-transparent/10 z-10 backdrop-blur-sm bg-background/80">
       <nav className="grid grid-cols-3 items-center p-1 px-4 text-md font-bold">
@@ -25,12 +27,12 @@ export async function TopNav({ signedIn = false }) {
           </Button>
         </div>
         <section className="grid justify-end space-x-4 items-center group-active:bg-background/70 ">
-          {signedIn ? (
+          {session ? (
+            <AvatarDropdown />
+          ) : (
             <Button asChild size="sm">
               <Link href="/signin">Sign in</Link>
             </Button>
-          ) : (
-            <AvatarDropdown />
           )}
         </section>
       </nav>
