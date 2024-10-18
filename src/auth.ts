195a5ab98efe54d256dd157565dc8fb82d6f793a
Lucas <developer.lucas@pm.me>
2024-10-18 11:51:39 -0400

    feat: added drizzle and created schema

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { db } from "@/db/index";
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: DrizzleAdapter(db),
   providers: [],
 });
