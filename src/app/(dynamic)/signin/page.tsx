610edaa9404eaddebbf5933a01b85bb9563865cb
Lucas <developer.lucas@pm.me>
2024-10-30 19:16:04 -0400

    feat: added demo account to sign in page

--- src/app/(dynamic)/signin/page.tsx
+++ src/app/(dynamic)/signin/page.tsx
@@ -1,19 +1,31 @@
-import { SignInForm } from "@/components/signin-form";
+import { SignInForm } from "../_components/signin-form";
 import Link from "next/link";
+
 export default async function SignIn() {
-  // const session = await auth();
-  // if (!session) {
-  //   console.log(session);
-  // }
   return (
-    <div className="sm:mx-auto sm:w-full sm:max-w-sm bg-gray-800/75 px-14 py-8 rounded-lg shadow-2xl backdrop-blur">
-      <div className="flex flex-col space-y-3">
-        <SignInForm />
-        <span className="font-bold text-lg text-center">or</span>
-        <Link href="/signup" className="mx-auto">
-          Create an Account
-        </Link>
+    <>
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm bg-gray-800/75 px-14 py-8 rounded-lg shadow-2xl backdrop-blur">
+        <div className="flex flex-col space-y-3">
+          <SignInForm />
+          <span className="font-bold text-lg text-center">or</span>
+          <Link href="/signup" className="mx-auto">
+            Create an Account
+          </Link>
+        </div>
       </div>
-    </div>
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm bg-gray-800/75 px-14 py-8 rounded-lg shadow-2xl backdrop-blur mt-4">
+        <div className="font-bold flex flex-col space-y-3 mb-2">
+          Demo Account
+        </div>
+        <div>
+          Email:
+          <span className="font-bold"> demo@lucasanderson.dev</span>
+        </div>
+        <div>
+          Password:
+          <span className="font-bold"> 12345678</span>
+        </div>
+      </div>
+    </>
   );
 }
