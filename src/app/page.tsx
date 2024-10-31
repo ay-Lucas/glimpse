916c4a3321f19238a1b0e4db6ba0eb58e47dd93c
Lucas <developer.lucas@pm.me>
2024-10-30 22:07:45 -0400

    refactor: deleted unreachable code and unnecessary function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,5 @@
 import { permanentRedirect } from "next/navigation";
 
 export default async function HomePage() {
-  function redirect() {
-    permanentRedirect("/discover");
-  }
-  redirect();
-
-  return (
-    <main className="relative">
-      <div className="relative flex h-screen flex-col md:flex-row md:overflow-hidden"></div>
-    </main>
-  );
+  permanentRedirect("/discover");
 }
