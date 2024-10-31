4a26d513eecf4e68ba4987c800124ea13880b9ec
Lucas <developer.lucas@pm.me>
2024-10-30 22:36:15 -0400

    fix: solved middleware preventing navigation to movies and series pages

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -1,3 +1,10 @@
-export const ROOT = "/";
-export const PUBLIC_ROUTES = ["/", "/discover", "/signin", "/signup"];
+export const ROOT = "/discover";
+export const PUBLIC_ROUTES = [
+  "/",
+  "/discover",
+  "/signin",
+  "/signup",
+  "/discover/movies",
+  "/discover/series",
+];
 export const DEFAULT_REDIRECT = "/protected";
