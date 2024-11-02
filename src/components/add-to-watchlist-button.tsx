228e983e86586ea3a6af3dc25171f773ae0cf0ed
Lucas <developer.lucas@pm.me>
2024-11-02 13:07:08 -0400

    feat: added add to watchlist button

---
+++ src/components/add-to-watchlist-button.tsx
@@ -0,0 +1,52 @@
+"use client";
+import { addToDefaultWatchlist } from "@/lib/actions";
+import { Button } from "./ui/button";
+import Link from "next/link";
+
+export async function AddToWatchlistButton({
+  userId,
+  title,
+  itemType,
+  watchlistName = "Default",
+  isLoggedIn,
+  tmdbId,
+  genres,
+}: {
+  userId: string | undefined;
+  title: string;
+  itemType: string;
+  watchlistName: string;
+  isLoggedIn: boolean;
+  tmdbId: number;
+  genres: string[];
+}) {
+  // console.log(
+  //   `user ID: ${userId}\nitemName: ${title} \nitemType: ${itemType} \nwatchListName: ${watchlistName} \nisLoggedIn: ${isLoggedIn} \ntmdbId ${tmdbId}`,
+  // );
+  return (
+    <>
+      {isLoggedIn &&
+      userId !== undefined &&
+      (itemType === "tv" || itemType === "movie") ? (
+        <form
+          action={async () =>
+            (itemType === "tv" || itemType === "movie") &&
+            (await addToDefaultWatchlist(
+              userId,
+              tmdbId,
+              title,
+              itemType,
+              genres,
+            ))
+          }
+        >
+          <Button variant="secondary">Add to Watchlist</Button>
+        </form>
+      ) : (
+        <Link href={"/signin"}>
+          <Button variant="secondary">Add to Watchlist</Button>
+        </Link>
+      )}
+    </>
+  );
+}
