8e7c554c3c8ee4396d1a5d2f749dda74289d4c09
Lucas <developer.lucas@pm.me>
2024-11-03 12:11:07 -0500

    feat: improved watchlist styling

--- src/components/watchlist.tsx
+++ src/components/watchlist.tsx
@@ -5,38 +5,53 @@ import { WatchlistDropdown } from "./watchlist-dropdown";
 
 export function Watchlist({ watchlist }: { watchlist: WatchlistI }) {
   return (
-    <div className="p-3 bg-secondary/40 rounded-2xl backdrop-blur-3xl">
+    <div className="p-3 bg-background border-secondary border rounded-2xl backdrop-blur-3xl">
       {watchlist ? (
         <>
-          <h2 className="text-xl text-center">{watchlist.watchlistName}</h2>
           {watchlist.items.length > 0 ? (
-            <div className="w-full p-2">
-              <div className="space-y-2">
-                {watchlist.items.map((item) => (
-                  <div
-                    className="grid grid-cols-3 justify-items-stretch bg-secondary/50 hover:bg-secondary/60 transition duration-100 p-3 rounded-xl  items-center space-x-5"
-                    key={item.id}
-                  >
-                    <Link href={`/${item.itemType}/${item.tmdbId}`}>
-                      <div>{item.title}</div>
-                    </Link>
-                    <div className="gap-x-1.5 gap-y-1 w-full flex flex-row">
-                      {item.genres?.map((genre, index: number) => (
-                        <ul
-                          key={index}
-                          className="bg-gray-700/60 shadow-lg rounded-lg px-2 py-1 select-none transition hover:bg-gray-700"
-                        >
-                          {genre}
-                        </ul>
-                      ))}
-                    </div>
-                    <div className="justify-self-end">
-                      <WatchlistDropdown
-                        watchlistId={item.watchlistId}
-                        watchlistItemId={item.itemId}
-                      />
-                    </div>
+            <div className="w-full p-3">
+              <h2 className="text-2xl text-center border-secondary">
+                {watchlist.watchlistName}
+              </h2>
+              <div className="grid space-y-2">
+                <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 px-3 py-2 items-center justify-items-start">
+                  <div className="col-span-1">
+                    <span className="text-gray-500">Title</span>
                   </div>
+                  <div className="col-span-1">
+                    <span className="text-gray-500">Genres</span>
+                  </div>
+                  {/* <div className="col-span-1"> */}
+                  {/*   <span className="text-gray-500">Rating</span> */}
+                  {/* </div> */}
+                </div>
+                {watchlist.items.map((item) => (
+                  <>
+                    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 border-secondary border p-3 rounded-xl items-center justify-items-start hover:border-primary/20 transition">
+                      <Link
+                        href={`/${item.itemType}/${item.tmdbId}`}
+                        className=""
+                      >
+                        <div>{item.title}</div>
+                      </Link>
+                      <div className="col-span-2 flex space-x-2">
+                        {item.genres?.map((genre, index: number) => (
+                          <ul
+                            key={index}
+                            className="bg-gray-700/60 shadow-lg rounded-lg px-2 py-1 select-none transition hover:bg-gray-700"
+                          >
+                            {genre}
+                          </ul>
+                        ))}
+                      </div>
+                      <div className="justify-self-end">
+                        <WatchlistDropdown
+                          watchlistId={item.watchlistId}
+                          watchlistItemId={item.itemId}
+                        />
+                      </div>
+                    </div>
+                  </>
                 ))}
               </div>
             </div>
