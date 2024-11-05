49c5f039099db0f0d7db9a6b7f21e5954dd9774b
Lucas <developer.lucas@pm.me>
2024-11-05 14:18:15 -0500

    feat: added watchlist to navbar, removed series and movies buttons

--- src/components/top-nav.tsx
+++ src/components/top-nav.tsx
@@ -3,6 +3,14 @@ import { Button } from "@/components/ui/button";
 import { AvatarDropdown } from "./avatar-dropdown";
 import { Search } from "./search";
 import { auth } from "@/auth";
+import {
+  LucideFileArchive,
+  LucideList,
+  LucideMailPlus,
+  LucidePlusSquare,
+  LucideSearch,
+  LucideTv,
+} from "lucide-react";
 
 export async function TopNav() {
   const session = await auth();
@@ -16,15 +24,18 @@ export async function TopNav() {
           <Search />
         </section>
         <div className="hidden md:flex md:opacity-100 items-center justify-center">
-          <Button variant="ghost" className="md:text-lg">
-            <Link href="/discover">Discover</Link>
-          </Button>
-          <Button variant="ghost" className="md:text-lg">
-            <Link href="/discover/movies">Movies</Link>
-          </Button>{" "}
-          <Button variant="ghost" className="md:text-lg">
-            <Link href="/discover/series">Series</Link>
-          </Button>
+          <Link href="/discover" className="items-center">
+            <Button variant="ghost" className="md:text-lg font-semibold">
+              <LucideTv className="mr-2 p-0.5 mb-1" />
+              Discover
+            </Button>
+          </Link>
+          <Link href={"/watchlist"} className="items-center">
+            <Button className="md:text-lg" variant="ghost">
+              <LucideList className="mr-2" />
+              Watchlist
+            </Button>
+          </Link>
         </div>
         <section className="grid justify-end space-x-4 items-center group-active:bg-background/70 ">
           {session ? (
