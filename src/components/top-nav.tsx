773f6cc46ae2acb443f60e477164b681ee73a0ed
Lucas <developer.lucas@pm.me>
2024-11-08 16:00:01 -0500

    feat(navbar): adapt navbar to mobile screens closes #2

--- src/components/top-nav.tsx
+++ src/components/top-nav.tsx
@@ -3,37 +3,30 @@ import { Button } from "@/components/ui/button";
 import { AvatarDropdown } from "./avatar-dropdown";
 import { Search } from "./search";
 import { auth } from "@/auth";
-import {
-  LucideFileArchive,
-  LucideList,
-  LucideMailPlus,
-  LucidePlusSquare,
-  LucideSearch,
-  LucideTv,
-} from "lucide-react";
+import { LucideList, LucideTv } from "lucide-react";
 
 export async function TopNav() {
   const session = await auth();
   return (
     <div className="sticky top-0 left-0 border-b border-transparent/10 z-10 backdrop-blur-sm bg-background/80">
       <nav className="grid grid-cols-3 items-center p-1 px-4 text-md font-bold">
-        <section className="col-span-2 md:col-span-1 flex z-10 items-center lg:space-x-5 ">
+        <section className="flex z-10 items-center lg:space-x-5 ">
           <Link className="hidden lg:inline" href="/">
             Glimpse
           </Link>
           <Search />
         </section>
-        <div className="hidden md:flex md:opacity-100 items-center justify-center">
+        <div className="flex flex-nowrap md:opacity-100 items-center justify-center">
           <Link href="/discover" className="items-center">
-            <Button variant="ghost" className="md:text-lg font-semibold">
-              <LucideTv className="mr-2 p-0.5 mb-1" />
-              Discover
+            <Button variant="ghost" className="sm:text-lg font-semibold">
+              <LucideTv className="sm:mr-2 p-0.5 mb-1" />
+              <span className="hidden sm:flex">Discover</span>
             </Button>
           </Link>
           <Link href={"/watchlist"} className="items-center">
-            <Button className="md:text-lg font-semibold" variant="ghost">
-              <LucideList className="mr-2" />
-              Watchlist
+            <Button className="sm:text-lg font-semibold" variant="ghost">
+              <LucideList className="sm:mr-2" />
+              <span className="hidden sm:flex">Watchlist</span>
             </Button>
           </Link>
         </div>
