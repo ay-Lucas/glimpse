5342e88ac81d6e1791cd6d777fa7ecade2d1b021
Lucas <developer.lucas@pm.me>
2024-11-14 18:36:58 -0500

    fix(backdrop): refactor getBackgrounds to correctly check popularity and add image urls to array

--- src/app/(dynamic)/(auth)/_components/backdrops.tsx
+++ src/app/(dynamic)/(auth)/_components/backdrops.tsx
@@ -1,27 +1,35 @@
 import { shuffle } from "@/lib/utils";
-import { baseApiUrl, options } from "@/lib/constants";
+import { baseApiUrl, baseImageUrl, options } from "@/lib/constants";
+import {
+  MovieResult,
+  MovieResultsResponse,
+  TrendingResponse,
+  TvResult,
+  TvResultsResponse,
+} from "@/types/request-types";
 
 const trendingTvUrl = `${baseApiUrl}/trending/tv/day?language=en-US`;
 const trendingMoviesUrl = `${baseApiUrl}/trending/movie/day?language=en-US`;
 const MIN_POPULARITY = 10;
 
 export async function getBackgrounds() {
-  let [res1, res2] = await Promise.all([
-    fetch(trendingTvUrl, options).then((response) => response.json()),
-    fetch(trendingMoviesUrl, options).then((response) => response.json()),
-  ]);
+  let [res1, res2]: [TvResultsResponse, MovieResultsResponse] =
+    await Promise.all([
+      fetch(trendingTvUrl, options).then((response) => response.json()),
+      fetch(trendingMoviesUrl, options).then((response) => response.json()),
+    ]);
 
-  const backdropUrls = [];
-  for (let i in res1.results) {
-    if (
-      res1.results[i].popularity > MIN_POPULARITY &&
-      res2.results[i].popularity > MIN_POPULARITY
-    )
-      backdropUrls.push(
-        `https://image.tmdb.org/t/p/original${res1.results[i].backdrop_path}`,
-        `https://image.tmdb.org/t/p/original${res2.results[i].backdrop_path}`,
-      );
-  }
+  const backdropUrls: string[] = [];
+
+  res1.results?.forEach((item) => {
+    if (item.popularity! > MIN_POPULARITY)
+      backdropUrls.push(`${baseImageUrl}${item.backdrop_path}`);
+  });
+
+  res2.results?.forEach((item) => {
+    if (item.popularity! > MIN_POPULARITY)
+      backdropUrls.push(`${baseImageUrl}${item.backdrop_path}`);
+  });
 
   // shuffle(backdropUrls);
   return backdropUrls;
