e8963e1bc39e78f4ca63964b996ed1f423a39112
Lucas <developer.lucas@pm.me>
2024-11-28 18:51:38 -0500

    fix(poster): stop lighthouse from throwing errors about no LCP by commenting out Poster Image placeholder and blurDataUrl

--- src/app/(dynamic)/[type]/_components/poster.tsx
+++ src/app/(dynamic)/[type]/_components/poster.tsx
@@ -15,8 +15,8 @@ export function Poster({
       sizes="(max-width: 460px) 50vw, (max-width: 768px) 30vw, (max-width: 1200px) 20vw, 10vw"
       className={`object-cover rounded-xl mx-auto w-[190px] h-[285px]`}
       priority
-      placeholder="blur"
-      blurDataURL={blurDataUrl}
+      // placeholder="blur"
+      // blurDataURL={blurDataUrl}
       loading="eager"
       alt="poster image"
     />
