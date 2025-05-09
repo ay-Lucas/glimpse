b55ddd881812bd5b5f482165f9b862794d5a0620
Lucas <developer.lucas@pm.me>
2025-05-09 14:46:07 -0400

    chore: update robots.ts file to exclude well-known crawlers

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,11 +1,53 @@
 import type { MetadataRoute } from "next";
 
+export const dynamic = "force-static";
+
+const abusiveBots = [
+  "AhrefsBot*",
+  "SemrushBot*",
+  "DotBot*",
+  "MJ12bot*",
+  "BLEXBot*",
+  "Sogou*",
+  "Exabot*",
+  "MegaIndex*",
+  "Yandex*",
+  "Baiduspider*",
+  "PetalBot*",
+  "CensysInspect*",
+  "Bytespider*",
+  "Amazonbot*",
+  "Seekport*",
+  "SeznamBot*",
+  "Qwantify*",
+  "MauiBot*",
+  "Screaming Frog SEO Spider*",
+  "ZoominfoBot*",
+  "BomboraBot*",
+  "SISTRIX Crawler*",
+  "DataForSeoBot*",
+  "Googlebot-Image*",
+  "GPTBot*",
+  "ClaudeBot*",
+];
+
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-      disallow: ["/tv/", "/movie/", "/search"],
-    },
+    // sitemap: "https://yourdomain.com/sitemap.xml",
+    rules: [
+      ...abusiveBots.map((ua) => ({ userAgent: ua, disallow: "/" })),
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: [
+          "/movie/",
+          "/tv/",
+          "/person/",
+          "/watchlist/",
+          "/api/",
+          "/search",
+        ],
+      },
+    ],
   };
 }
