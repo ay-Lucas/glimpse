9e4af4c10cf1feaa880f354fd3378b05bda90812
Lucas <developer.lucas@pm.me>
2025-05-14 13:58:30 -0400

    fix: replace review read more label with unique id's to expand correctly when multiple reviews are present

--- src/app/(dynamic)/[type]/_components/reviews.tsx
+++ src/app/(dynamic)/[type]/_components/reviews.tsx
@@ -23,20 +23,20 @@ export function Review({ data }: { data: ReviewI }) {
         )}
       </div>
       <div className="relative">
-        <input id="bio-toggle" type="checkbox" className="peer sr-only" />
+        <input id={data.id} type="checkbox" className="peer sr-only" />
         <p className="overflow-hidden max-h-[4.5rem] peer-checked:max-h-[1000px] transition-all duration-300 ease-in-out">
           {data.content}
         </p>
         <label
-          htmlFor="bio-toggle"
+          htmlFor={data.id}
           className="py-1 text-blue-600 cursor-pointer select-none hover:underline block peer-checked:hidden peer-checked:aria-expanded:false"
           aria-expanded="true"
         >
           Read more
         </label>
         <label
-          htmlFor="bio-toggle"
-          className="py-1 mt-2 text-blue-600 cursor-pointer select-none hover:underline hidden peer-checked:block peer-checked:aria-expanded=true"
+          htmlFor={data.id}
+          className="py-1 text-blue-600 cursor-pointer select-none hover:underline hidden peer-checked:block peer-checked:aria-expanded=true"
           aria-expanded="false"
         >
           Show less
