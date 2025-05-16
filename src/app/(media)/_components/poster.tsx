4dfb868d558a9a4fe310f63a0ae48203fb9ba011
Lucas <developer.lucas@pm.me>
2025-05-16 12:27:44 -0400

    perf: optimize poster by reducing image quality from 75 to 60 and remove size prop

--- src/app/(media)/_components/poster.tsx
+++ src/app/(media)/_components/poster.tsx
@@ -8,15 +8,14 @@ export function Poster({
 }) {
   return (
     <Image
-      quality={75}
+      quality={60}
       width={190}
       height={285}
       src={src}
-      sizes="(max-width: 460px) 50vw, (max-width: 768px) 30vw, (max-width: 1200px) 20vw, 10vw"
       className={`object-cover rounded-xl mx-auto w-[190px] h-[285px]`}
       priority
-      // placeholder="blur"
-      // blurDataURL={blurDataUrl}
+      placeholder="blur"
+      blurDataURL={blurDataUrl}
       loading="eager"
       alt="poster image"
     />
