44eebfdac93a2f7ac5b23a3351c80e7fc92316df
Lucas <developer.lucas@pm.me>
2025-06-11 12:59:01 -0400

    fix: reduce revalidate/api maxDuration to 60s for revalidate route to meet Vercel limits

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -4,7 +4,7 @@ import { revalidatePath } from "next/cache";
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 export const runtime = "nodejs";
-export const maxDuration = 180;
+export const maxDuration = 60; // default = 60, Vercel Hobby Plan limit is 60s
 
 export async function POST(req: Request) {
   const { secret, paths } = await req.json();
