67a80469ec1b163e5cf71590d7c86d074942ebaa
Lucas <developer.lucas@pm.me>
2025-06-22 15:47:54 -0400

    fix(provider): correct additional provider button number to open modal, improve layout on mobile screen, remove amber color from resolution icon.

--- src/app/(media)/_components/provider-list.tsx
+++ src/app/(media)/_components/provider-list.tsx
@@ -12,22 +12,22 @@ import {
 } from "@/components/ui/dialog"; // adjust path if needed
 import Image from "next/image";
 import { BsBadge4K, BsBadgeHd, BsBadgeSd } from "react-icons/bs";
-import { GroupedProvider } from "../actions";
 import { sortLanguages } from "@/lib/lang-utils";
 import { GiHeadphones } from "react-icons/gi";
 import { BsCcSquare } from "react-icons/bs";
+import { GroupedProvider } from "@/types/camel-index";
 
-export default function JustWatchProviderList({ info: providers }: { info: GroupedProvider[] }) {
+export default function JustWatchProviderList({ info }: { info: GroupedProvider[] }) {
   const MAX_INLINE = 4;
   const [open, setOpen] = useState(false);
-  const flatStreams: StreamProvider[] = providers.flatMap(item => ungroupProvider(item, true));
-  const inlineProviders = flatStreams.slice(0, MAX_INLINE);
+  const providers: StreamProvider[] = info.flatMap(item => ungroupProvider(item, true));
+  const inlineProviders = providers.slice(0, MAX_INLINE);
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <ul className="space-y-1 w-full">
         {inlineProviders.map((p) => (
-          <ProviderCard provider={p} key={p.Provider} />
+          <ProviderCard provider={p} key={`${p.Provider}:${p.Type}`} />
         ))}
       </ul>
       {providers.length > MAX_INLINE && (
@@ -40,8 +40,8 @@ export default function JustWatchProviderList({ info: providers }: { info: Group
       <DialogContent className="max-w-[95vw] sm:max-w-[90vw] lg:max-w-5xl max-h-[90vh] overflow-y-auto">
         <DialogTitle>All Providers</DialogTitle>
         <ul className="space-y-0">
-          {flatStreams.map((p) => (
-            <ModalProviderCard provider={p} key={p.Provider} />
+          {providers.map((p) => (
+            <ModalProviderCard provider={p} key={`${p.Provider}:${p.Type}`} />
           ))}
         </ul>
         <div className="mt-4 text-right">
@@ -84,56 +84,57 @@ function ModalProviderCard({ provider }: { provider: StreamProvider }) {
   const resolution = Resolution === "4K" || Resolution === "HD" || Resolution === "SD" ? Resolution : null
 
   return (
-    <li key={providerName} className="hover:bg-muted/70 duration-200 bg-transparent border" >
-      <a
-        href={url}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="grid grid-cols-2 sm:grid-cols-[1fr_125px]
-                   items-center gap-4 p-2"
-      >
-        <div className={`grid ${isCinemaType ? "grid-cols-2" : "grid-cols-3"} gap-5 items-center`}>
-          <Image
-            src={iconUrl}
-            alt={`${Name} logo`}
-            width={140}
-            height={48}
-            className="rounded object-cover"
-          />
-
-          {(Resolution || audioList || subtitleList) && (
-            <div className="flex flex-col space-y-1 text-sm">
-              {resolution && <span>{getResolutionIcon(resolution, 20, "text-amber-300")}</span>}
-              {audioList && (
-                <div className="flex space-x-2">
-                  <div>
-                    <GiHeadphones size={20} />
-                  </div>
-                  <span className="text-xs text-gray-400">{audioList}</span>
+    <li key={providerName} className="hover:bg-muted/70 duration-200 bg-transparent border p-1" >
+      <div className="grid grid-cols-[minmax(100px,150px),1fr] items-center">
+        <div className="">
+          {iconUrl &&
+            <Image
+              src={iconUrl}
+              alt={`${Name} logo`}
+              width={100}
+              height={40}
+              className="rounded object-cover"
+            />
+          }
+        </div>
+        <div className="grid gap-4 grid-cols-[minmax(150px,600px),minmax(20px,50px),minmax(103px,150px)] sm:grid-cols-[minmax(150px,600px),minmax(20px,50px),minmax(80px,150px),minmax(103px,125px)] p-2">
+          <div className="flex space-x-2 items-center">
+            {audioList && (
+              <>
+                <div>
+                  <GiHeadphones size={22} />
                 </div>
-              )}
-              {subtitleList && (
-                <div className="flex space-x-2">
-                  <div>
-                    <BsCcSquare size={20} />
-                  </div>
-                  <span className="text-xs text-gray-400">{subtitleList}</span>
+                <span className="text-xs text-gray-400">{audioList}</span>
+              </>
+            )}
+          </div>
+          <div className="flex space-x-2 items-center row-start-2">
+            {subtitleList && (
+              <>
+                <div>
+                  <BsCcSquare size={22} />
                 </div>
-              )}
-            </div>
-          )}
+                <span className="text-xs text-gray-400">{subtitleList}</span>
+              </>
+            )}
+          </div>
+          <div className="">
+            {resolution && <span>{getResolutionIcon(resolution, 22)}</span>}
+          </div>
 
-          <div className={`flex flex-col text-sm ${isCinemaType ?? "col-start-2 row-start-1"}`}>
+          <div className={`flex flex-col text-sm sm:pl-0 pl-3`}>
             <span className="font-medium">{typeLabel}</span>
             {Price && <span className="text-xs text-muted-foreground">{Price}</span>}
           </div>
+          <div className="text-left sm:col-start-4 col-start-3">
+            <a href={url}>
+              <Button size="sm" variant="outline">
+                {ctaLabel}
+              </Button>
+            </a>
+          </div>
         </div>
-        <div className="text-left">
-          <Button size="sm" variant="outline">
-            {ctaLabel}
-          </Button>
-        </div>
-      </a>
+      </div>
     </li>
   );
 }
@@ -165,11 +166,10 @@ function ProviderCard({ provider }: { provider: StreamProvider }) {
     : null;
 
   const isCinemaType = Type === "CINEMA";
-  console.log(Resolution)
   return (
     <li className={`grid grid-cols-[1fr_103px] gap-5 px-2 py-2 items-center rounded-xl text-sm hover:bg-muted/70 duration-200`}>
       <DialogTrigger asChild>
-        <button className={`grid ${isCinemaType ? "grid-cols-2" : "grid-cols-3"} gap-5 items-center`}>
+        <button className={`grid grid-cols-3 gap-5 items-center`}>
           <Image
             src={iconUrl}
             alt={Name}
@@ -177,17 +177,21 @@ function ProviderCard({ provider }: { provider: StreamProvider }) {
             height={32}
             className="rounded object-cover"
           />
-          {((resolution || audioList) && (
-            <div className="flex flex-col text-sm truncate">
-              {resolution && <span>{getResolutionIcon(resolution, 22)}</span>}
-              {audioList && (
-                <div className="flex space-x-2">
-                  <GiHeadphones />
-                  <span className="truncate text-left text-gray-400 text-xs">{audioList}</span>
-                </div>
-              )}
-            </div>
-          ))}
+          <div className="flex flex-col text-sm truncate">
+            {((resolution || audioList) && (
+              <>
+                {resolution && <span>{getResolutionIcon(resolution, 22)}</span>}
+                {audioList && (
+                  <div className="flex space-x-2">
+                    <div>
+                      <GiHeadphones />
+                    </div>
+                    <span className="truncate text-left text-gray-400 text-xs">{audioList}</span>
+                  </div>
+                )}
+              </>
+            ))}
+          </div>
           <div className="flex flex-col items-start whitespace-nowrap">
             <span className="">{typeLabel}</span>
             {Price && <span className="text-xs text-gray-400">{Price}</span>}
