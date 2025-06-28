7c98e402ebff0f86be86872f0231c544fc835ec8
Lucas <developer.lucas@pm.me>
2025-06-28 12:50:13 -0400

    chore: add console.warn if postgres environment variables are absent

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,17 @@
 import "dotenv/config";
 import { defineConfig } from 'drizzle-kit';
 
+const url = process.env.POSTGRES_URL_SESSION_POOLER!;
+
+if (!url) {
+  console.warn("POSTGRES_URL_SESSION_POOLER is missing or undefined")
+}
+
 export default defineConfig({
   out: './drizzle',
   schema: './src/db/schema.ts',
   dialect: 'postgresql',
-  dbCredentials: {
-    url: process.env.POSTGRES_URL_SESSION_POOLER!,
-  },
+  dbCredentials: { url: url },
   verbose: true,
   strict: true
 });
--- src/db/index.ts
+++ src/db/index.ts
@@ -4,6 +4,10 @@ import * as schema from "./schema"
 
 const connectionString = process.env.POSTGRES_URL!;
 
+if (!connectionString) {
+  console.warn("POSTGRES_URL is missing or undefined")
+}
+
 // Disable prefetch as it is not supported for "Transaction" pool mode
 export const client = postgres(connectionString!, { prepare: false })
 export const db = drizzle(client, { schema });
