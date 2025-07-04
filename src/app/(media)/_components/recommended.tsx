4a15182d228e51ccc4a633ad76100a8e706eb7de
Lucas <developer.lucas@pm.me>
2025-07-04 18:47:19 -0400

    chore(carousel): replace Recommendations ImageCarousel with MediaCarousel

--- src/app/(media)/_components/recommended.tsx
+++ src/app/(media)/_components/recommended.tsx
@@ -1,62 +1,36 @@
 import { MovieResult, TvResult } from "@/types/request-types-snakecase";
-import ImageCarousel from "@/components/image-carousel";
-import Link from "next/link";
 import { Card } from "@/components/card";
-import { BASE_POSTER_IMAGE_URL, BaseImageUrl } from "@/lib/constants";
-import { appendBlurDataToMediaArray } from "@/lib/blur-data-generator";
+import { BASE_POSTER_IMAGE_URL } from "@/lib/constants";
+import MediaCarousel from "@/components/media-carousel";
 
 export async function Recommended({
   recommendations,
 }: {
   recommendations: Array<MovieResult | TvResult>;
 }) {
-  const posterPaths = recommendations.map((item) => item.poster_path);
-  const items = (await appendBlurDataToMediaArray(
-    recommendations,
-    BaseImageUrl.BLUR,
-    posterPaths
-  )) as Array<MovieResult | TvResult>;
+  const validItems = recommendations.filter((item) => item.poster_path);
+
+  const items = validItems.map(
+    (item: MovieResult | TvResult, index: number) => {
+      const title = (item as any).title || (item as any).name;
+      return (
+        <Card
+          title={title}
+          overview={item.overview}
+          imagePath={item.poster_path}
+          baseUrl={BASE_POSTER_IMAGE_URL}
+          key={index}
+        />
+      );
+    }
+  );
   return (
     <>
       {items?.length > 0 && (
-        <ImageCarousel
-          title={<h2 className={`text-2xl font-bold`}>Recommended</h2>}
-          items={items.map((item: MovieResult | TvResult, index: number) => {
-            if (!item.poster_path) return;
-            let card: React.ReactNode;
-            switch (item.media_type) {
-              case "movie":
-                card = (
-                  <Card
-                    title={item.title}
-                    overview={item.overview}
-                    imagePath={item.poster_path}
-                    baseUrl={BASE_POSTER_IMAGE_URL}
-                    blurDataURL={(item as any).blurDataURL}
-                  />
-                );
-                break;
-              case "tv":
-                card = (
-                  <Card
-                    title={item.name}
-                    overview={item.overview}
-                    imagePath={item.poster_path}
-                    baseUrl={BASE_POSTER_IMAGE_URL}
-                    blurDataURL={(item as any).blurDataURL}
-                  />
-                );
-                break;
-            }
-            return (
-              <Link href={`/${item.media_type}/${item.id}`} key={index}>
-                {card}
-              </Link>
-            );
-          })}
-          loading="lazy"
-          breakpoints="page"
-        />
+        <div className="space-y-4">
+          <h2 className={`text-2xl font-bold`}>Recommended</h2>
+          <MediaCarousel items={items} breakpointType="poster" />
+        </div>
       )}
     </>
   );
